6b16e0387ead5d2c2d54a777dc2d958fae8cd51e
Dan <dan@ramdankorkelia.com>
2018-08-31 12:24:58 +0100

    Initial Commit

---
+++ TodayWidgetAndUIActivityViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayWidgetAndUIActivityViewController
+//
+//  Created by Dan Korkelia on 31/08/2018.
+//  Copyright © 2018 Dan Korkelia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
